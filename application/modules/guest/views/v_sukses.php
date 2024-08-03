60cc7d9f7c2e98884123908f550c18d28c04a406
taufiqadityaputra <taufiqadityaputra5@gmail.com>
2024-08-03 10:10:44 +0700

    hh

--- application/modules/guest/views/v_sukses.php
+++ application/modules/guest/views/v_sukses.php
@@ -1,22 +1,11 @@
 <div class="col-12 d-flex justify-content-center align-items-center" style="height: 80vh;">
     <div class="row w-100">
         <div class="col-lg-12 d-flex justify-content-center align-items-center">
-            <div class="card shadow-2">
-                <div class="card-body">
-                    <div class="d-flex flex-column justify-content-center align-items-center">
-                        <lottie-player src="https://lottie.host/6aad91b1-fdbd-4f8d-ad6d-a6ccdac2a185/Y8ChEV7zAc.json"
-                            background="transparent"
-                            speed="1"
-                            style="width: 300px; height: 300px;"
-                            loop
-                            autoplay>
-                        </lottie-player>
-                        <div class="card card-bordered card-body">
-                            <span class="fs-30 fw-600 text-center">Berhasil !!!</span>
-                            <span class="fs-15 mb-2 text-center">Informasi selanjutnya akan disampaikan melalui Whatsapp.</span>
-                        </div>
-                    </div>
-                </div>
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <lottie-player src="https://lottie.host/354dd7b4-3960-43b2-ba32-44626fdbe467/jzUCG69YCc.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay>
+                </lottie-player>
+                <span class="fs-30 fw-600 text-center">Berhasil !!!</span>
+                <span class="fs-15 mb-2 text-center">Informasi selanjutnya akan disampaikan melalui Whatsapp.</span>
             </div>
         </div>
     </div>
